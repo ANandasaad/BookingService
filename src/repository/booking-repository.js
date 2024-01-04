5f413d58ec4a747be4237dc008cb63eb8cdf1a58
Anand kushwaha <anandkushwaha@Anands-Mac-mini.local>
2024-01-05 00:13:56 +0530

    Add migration to add new column to bookings

--- src/repository/booking-repository.js
+++ src/repository/booking-repository.js
@@ -20,7 +20,17 @@ class BookingRepository {
   }
   async update() {
     try {
-    } catch (error) {}
+    } catch (error) {
+      if (error.name == "SequelizeValidationError") {
+        throw new ValidationError(error);
+      }
+      throw new AppError(
+        "RepositoryError",
+        "cannot create booking",
+        "There  was some issues while updating , please try again",
+        InternalServerError
+      );
+    }
   }
 }
 module.exports = BookingRepository;
