8d3f077a62206e86fb58e203e4d6fbf613a50d32
Anand kushwaha <anandkushwaha@Anands-Mac-mini.local>
2024-01-14 19:22:18 +0530

    api gateway

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ const db = require("./models/index");
 const setupAndStartServer = () => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
-  app.use("/api", apiRoutes);
+
+  app.use("/bookingservice/api", apiRoutes);
   app.listen(PORT, () => {
     console.log("listening on port " + PORT);
 
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,6 +3,11 @@ const {BookingController}= require("../../controllers/index")
 
 const bookingController= new BookingController()
 const router = express.Router();
+router.get("/info",(req,res)=>{
+    return res.json({
+        message:"Info ...Booking Service"
+    })
+})
 router.post("/bookings", bookingController.create);
 router.post("/publish" ,bookingController.sendMessageToQueue)
 module.exports = router;
